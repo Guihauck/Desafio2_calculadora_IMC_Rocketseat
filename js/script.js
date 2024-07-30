95ab8c1b2bbd440aa9ebf2fde4dc1efc37211423
Guihauck <guihauck1998@gmail.com>
2024-07-30 10:52:19 -0300

    Estruturação dos dados do Modal

--- js/script.js
+++ js/script.js
@@ -3,9 +3,25 @@
 const form = document.querySelector("form")
 const inputWeight = document.querySelector("#weight")
 const inputHeight = document.querySelector("#height")
-const modalWrapper = document.querySelector(".modal-wrapper")
-const modalMessage = document.querySelector(".modal .title span")
-const modalBtnClose = document.querySelector(".modal button.close")
+
+//const modalWrapper = document.querySelector(".modal-wrapper")
+//const modalMessage = document.querySelector(".modal .title span")
+//const modalBtnClose = document.querySelector(".modal .close-popup")
+
+//Estruturando dados do Modal
+
+const modal = {
+  wrapper: document.querySelector(".modal-wrapper"),
+  message: document.querySelector(".modal .title span"),
+  buttonClose: document.querySelector(".modal .close-popup"),
+
+  open() {
+    modal.wrapper.classList.add('open')
+  },
+  close() {
+    modal.wrapper.classList.remove('open')
+  }
+}
 
 // Funções
 
@@ -18,10 +34,14 @@ form.onsubmit = event => {
   const result = IMC(weight, height);
   const message = `Seu IMC é de ${result}`
 
-  modalMessage.innerText = message
-  modalWrapper.classList.add('open')
+  modal.message.innerText = message
+  modal.open()
 }
 
 let IMC = (weight, height) => {
-  return (weight / (height/100)**2).toFixed(2);
+  return (weight / (height / 100) ** 2).toFixed(2);
+}
+
+modal.buttonClose.onclick = () => {
+  modal.close()
 }
