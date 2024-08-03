c4eadb757964ea3738574aa920ad98fec8fee222
Guihauck <guihauck1998@gmail.com>
2024-08-03 10:10:20 -0300

    Modularização do projeto Modal

--- js/script.js
+++ js/script.js
@@ -1,27 +1,12 @@
+import {modal} from './modal.js'
+
 // Variáveis
 
 const form = document.querySelector("form")
 const inputWeight = document.querySelector("#weight")
 const inputHeight = document.querySelector("#height")
 
-//const modalWrapper = document.querySelector(".modal-wrapper")
-//const modalMessage = document.querySelector(".modal .title span")
-//const modalBtnClose = document.querySelector(".modal .close-popup")
 
-//Estruturando dados do Modal
-
-const modal = {
-  wrapper: document.querySelector(".modal-wrapper"),
-  message: document.querySelector(".modal .title span"),
-  buttonClose: document.querySelector(".modal .close-popup"),
-
-  open() {
-    modal.wrapper.classList.add('open')
-  },
-  close() {
-    modal.wrapper.classList.remove('open')
-  }
-}
 
 // Funções
 
@@ -42,6 +27,3 @@ let IMC = (weight, height) => {
   return (weight / (height / 100) ** 2).toFixed(2);
 }
 
-modal.buttonClose.onclick = () => {
-  modal.close()
-}
