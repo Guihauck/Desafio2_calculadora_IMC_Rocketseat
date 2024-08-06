3a0344104091b8b5ccc4449003cc2dc8b89fde89
Guihauck <guihauck1998@gmail.com>
2024-08-05 22:14:03 -0300

    validação de dados

--- js/script.js
+++ js/script.js
@@ -6,8 +6,6 @@ const form = document.querySelector("form")
 const inputWeight = document.querySelector("#weight")
 const inputHeight = document.querySelector("#height")
 
-
-
 // Funções
 
 form.onsubmit = event => {
@@ -16,6 +14,13 @@ form.onsubmit = event => {
   const weight = inputWeight.value
   const height = inputHeight.value
 
+  const showErrorNotNumber = isNotANumber(weight) || isNotANumber(height)
+
+  if (showErrorNotNumber) {
+    console.log('Existe o erro!');
+    return;
+  }
+ 
   const result = IMC(weight, height);
   const message = `Seu IMC é de ${result}`
 
@@ -27,3 +32,6 @@ let IMC = (weight, height) => {
   return (weight / (height / 100) ** 2).toFixed(2);
 }
 
+function isNotANumber (value) {
+   return isNaN(value) || value == ""
+}
